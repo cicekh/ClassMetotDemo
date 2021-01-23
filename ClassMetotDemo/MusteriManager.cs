2cb1fb04eebda7cf4cf243d0f4a2bbd895d18188
cicekh <haydarcicekci@hotmail.com>
2021-01-23 18:48:23 +0300

    first commit

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Add(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + " adlı müşteri eklendi");
+        }
+
+        public void Delete(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + " adlı müşteri silindi");
+        }
+
+        public void Listele(List<Musteri> musteriler)
+        {
+            Console.WriteLine("Müşterilerin listesi");
+            Console.WriteLine("--------------------");
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Ad + " " + musteri.Soyad);
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sistemde ekleme, silme, listeleme işlemi yapılır.");
+
+            Musteri musteri1 = new Musteri { Ad = "Hakan", Soyad = "Ayaz", Sehir = "Ankara", Id = 1 };
+            Musteri musteri2 = new Musteri { Ad = "Çağan", Soyad = "Memişoğlu", Sehir = "İstanbul", Id = 2 };
+
+            MusteriManager manager = new MusteriManager();
+            manager.Add(musteri1);
+            manager.Delete(musteri2);
+
+            List<Musteri> musteriListesi = new List<Musteri> { musteri1, musteri2 };
+            manager.Listele(musteriListesi);
+
+
+
+        }
+    }
+}
